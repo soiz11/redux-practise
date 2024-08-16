861c8fc29d7794e304c559ed7a667f0b12b8f8c2
Sohan Prabhath <soizsinghe@gmail.com>
2024-08-16 17:35:50 +0530

    initial commit

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
+
+const initialState = {
+  cartItems: cartItems,
+  amount: 3,
+  total: 0,
+  isLoading: true,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, action) => {
+      const itemId = action.payload;
+      const cartItem = state.cartItems.find((item) => item.id === itemId);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, action) => {
+      const itemId = action.payload;
+      const cartItem = state.cartItems.find((item) => item.id === itemId);
+      cartItem.amount = cartItem.amount - 1;
+    },
+  },
+});
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
+export default cartSlice.reducer;
